1e32ec660cbb541f3fff37f9dce42e69956aa2ef
Max Ponomarenko <JustForLobster@gmail.com>
2024-08-05 19:04:21 +0500

    Спринт 13. Сериализаторы

--- kittygram_plus/settings.py
+++ kittygram_plus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_l6az+#pc&#91+iy&p2zpf)!!dz7^dzsa6#8d_^(y@%8zi@1mv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
